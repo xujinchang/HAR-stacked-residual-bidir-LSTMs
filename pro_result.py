d874afbef67a6d1316fb9e23c38e9c120a73a713
xujinchang <xjc1@bupt.edu.cn>
2017-06-07 00:47:58 +0800

    produce the submmited result

---
+++ pro_result.py
@@ -0,0 +1,30 @@
+import os
+import pickle
+import pprint
+path = '/home/tmp_data_dir/zhuzezhou/codalab/CVal'
+list1 = os.listdir(path)
+list1 = sorted(list1)
+output = open('valid_prediction.pkl','wb')
+fp = open('test_label_result','r')
+dic = {}
+count = 0
+for line in fp.readlines():
+	line = line.strip().split('\n')
+	if int(line[0]) == 0:
+		dic[list1[count]] = 'fake'
+	else:
+		dic[list1[count]] = 'true'
+	count = count + 1
+pickle.dump(dic, output)
+
+output.close()
+fp.close()
+
+
+pkl_file = open('valid_prediction.pkl', 'rb')
+
+data1 = pickle.load(pkl_file)
+pprint.pprint(data1)
+
+pkl_file.close()
+
